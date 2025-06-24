2cdb23bf9c62f15b067d7a90c50327bc22f983df
Berry-mas <junseo9145@gmail.com>
2025-06-24 17:54:12 +0900

    20250624 수업 종료 커밋

--- 05_PARKJUNSEO/01_docker/docker-compose/chpa01-01-boot-project/src/main/java/com/smile/bootproject/config/WebConfig.java
+++ 05_PARKJUNSEO/01_docker/docker-compose/chpa01-01-boot-project/src/main/java/com/smile/bootproject/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // next.js에 설정된 3000번 포트에서 오는 cors 설정 해제
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                // NodePort에 설정된 30000번 포트에서 오는 연결에 대한 CQRS 설정 해제
+                  .allowedOrigins("http://localhost:30000")
                 // Ingress 설정으로, 내부적으로 통신이 수행되므로 cors 설정 없이 통신 가능
                 .allowedOrigins("")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
